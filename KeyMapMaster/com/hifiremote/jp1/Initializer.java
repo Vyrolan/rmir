d88f0cc6f981f8c9fe9cda1db08fe8d9833c3216
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2003-04-02 19:07:46 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@13 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- KeyMapMaster/com/hifiremote/jp1/Initializer.java
+++
@@ -1,7 +0,0 @@
-package com.hifiremote.jp1;
-
-public abstract class Initializer
-{
-  public abstract void initialize( DeviceParameter[] devParms,
-                                   CmdParameter[] cmdParms );
-}
