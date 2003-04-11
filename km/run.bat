6b6cb44c8b77a140af203c22e64e11f2036d5d41
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2003-04-11 01:45:42 +0000

    Added support for KMJ_HOME environment variable, to make it easier for testing outside of the source/build tree
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@36 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/run.bat
+++ km/run.bat
@@ -1,1 +1,5 @@
-java -cp .;classes com.hifiremote.jp1.KeyMapMaster
+@echo off
+if NOT _%KMJ_HOME% == _ goto homeset
+SET KMJ_HOME=.
+:homeset
+java -cp %KMJ_HOME%;%KMJ_HOME%/classes com.hifiremote.jp1.KeyMapMaster
