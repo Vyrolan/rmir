a8286f149a00b815a5989c2be0f0223114e01930
johnsfine <johnsfine@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2003-05-22 13:44:30 +0000

    Two byte version of G.I. 4DTV
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@114 bfc585dc-5255-483b-a5ca-a9bf5897b33d

---
+++ km/com/hifiremote/jp1/GI2.java
@@ -0,0 +1,28 @@
+package com.hifiremote.jp1;
+
+public class GI2
+  extends Translate
+{
+  public GI2( String[] textParms )
+  {
+    super( textParms );
+  }
+
+  public void in( Value[] parms, Hex hexData, DeviceParameter[] devParms, int onlyIndex )
+  {
+    byte[] hex = hexData.getData();
+    int dev = (( Integer )devParms[0].getValueOrDefault()).intValue();
+    int obc = (( Integer )parms[ 0 ].getUserValue()).intValue();
+    int cmd = (dev<<6)+obc;
+    hex[0] = reverse( (byte) (63-obc) );
+    int check = (cmd<<6) ^ (cmd<<3) ^ (cmd<<2) ^ cmd ^ (cmd>>2) ^ (cmd>>3) ^ (cmd>>4);
+    hex[1] = reverse( (byte)( 63 - dev - (check&56) - ((check>>4)&4) ) );
+  }
+
+  public void out( Hex hex, Value[] parms, DeviceParameter[] devParms )
+  {
+     parms[0] = new Value( new Integer( 63 - ( 63 & reverse( hex.getData()[0] ) ) ), null );
+  }
+
+}
+
