5f4cac5a474b5aefde567c473fc656ec0eae9b5e
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2003-08-06 16:43:56 +0000

    LayoutPanel: highlight button by clicking on it
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@193 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/com/hifiremote/jp1/LayoutPanel.java
+++ km/com/hifiremote/jp1/LayoutPanel.java
@@ -14,50 +14,121 @@ import info.clearthought.layout.*;
 public class LayoutPanel
   extends KMPanel
 {
-  public LayoutPanel( DeviceUpgrade deviceUpgrade )
+  public LayoutPanel( DeviceUpgrade devUpgrade )
   {
-    super( deviceUpgrade );
-    image = new ImageIcon( "15-1994.jpg" );
-  }
+    super( devUpgrade );
 
-  public void paint(Graphics g)
-  {
-    Graphics2D g2 = ( Graphics2D ) g;
-    g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, 
-                         RenderingHints.VALUE_ANTIALIAS_ON );
-    Remote r = deviceUpgrade.getRemote();
-    ImageIcon icon = r.getImageIcon();
-    if ( icon != null )
-      g2.drawImage( icon.getImage(), null, null );
-   
-    g2.setPaint( Color.blue );
-
-    DeviceType devType = deviceUpgrade.getDeviceType();
-    ButtonMap map = devType.getButtonMap();
-    // Button[] buttons = r.getButtons();
-    // for ( int i = 0; i < buttons.length; i++ )
-    // {
-      // Button b = buttons[ i ];
-      // if ( !map.isPresent( b ))
-      // {
-        // Shape shape = b.getShape();
-        // if ( shape != null )
-          // g2.fill( shape );
-      // }
-    // }
-
-    g2.setPaint( Color.yellow );
-    g2.setStroke( new BasicStroke( 2.0f ));
-    for ( int i = 0; i < map.size(); i++ )
+    imagePanel = new JPanel()
     {
-      Button b = map.get( i );
-      Shape s = b.getShape();
-      if ( s != null )
-        g2.draw( s );
-      else
-        System.err.println( "No shape for button " + b );
-    }
+      public void paint( Graphics g )
+      {
+        Graphics2D g2 = ( Graphics2D ) g;
+        g2.setRenderingHint( RenderingHints.KEY_ANTIALIASING, 
+                             RenderingHints.VALUE_ANTIALIAS_ON );
+        Remote r = deviceUpgrade.getRemote();
+        ImageIcon icon = r.getImageIcon();
+        if ( icon != null )
+          g2.drawImage( icon.getImage(), null, null );
+       
+        g2.setPaint( Color.blue );
+    
+        DeviceType devType = deviceUpgrade.getDeviceType();
+        ButtonMap map = devType.getButtonMap();
+        // Button[] buttons = r.getButtons();
+        // for ( int i = 0; i < buttons.length; i++ )
+        // {
+          // Button b = buttons[ i ];
+          // if ( !map.isPresent( b ))
+          // {
+            // Shape shape = b.getShape();
+            // if ( shape != null )
+              // g2.fill( shape );
+          // }
+        // }
+    
+        if ( currentButton != null )
+        {
+          g2.setPaint( Color.white );
+          g2.fill( currentButton.getShape());
+        }
+        g2.setPaint( Color.yellow );
+        g2.setStroke( new BasicStroke( 2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND ));
+        for ( int i = 0; i < map.size(); i++ )
+        {
+          Button b = map.get( i );
+          Shape s = b.getShape();
+          if ( s != null )
+            g2.draw( s );
+          else
+            System.err.println( "No shape for button " + b );
+        }
+      }
+    };
+    add( imagePanel );
+
+    imagePanel.addMouseListener( new MouseAdapter()
+    {
+      public void mousePressed( MouseEvent e )
+      {
+        Point p = e.getPoint();
+        Button savedButton = currentButton;
+        currentButton = null;
+        Button[] buttons = deviceUpgrade.getRemote().getButtons();
+        for ( int i = 0; i < buttons.length; i++ )
+        {
+          Button b = buttons[ i ];
+          Shape s = b.getShape();
+          if (( s != null ) && s.contains( p ))
+          {
+            currentButton = b;
+            break;
+          }
+        }
+        if ( currentButton != savedButton )
+          doRepaint();
+      }
+  
+      public void mouseReleased( MouseEvent e )
+      {
+        ;
+      }
+    });
   }
 
-  private ImageIcon image = null;
+  public void update()
+  {
+    ImageIcon icon = deviceUpgrade.getRemote().getImageIcon();
+    if ( icon != null )
+    {
+      int w = icon.getIconWidth();
+      int h = icon.getIconHeight();
+      Dimension size = new Dimension( w, h );
+      imagePanel.setPreferredSize( size );
+      imagePanel.setMaximumSize( size );
+      imagePanel.setMinimumSize( size );
+      imagePanel.setSize( w, h );
+    }
+    Button[] buttons = deviceUpgrade.getRemote().getButtons();
+    boolean found = false;
+    for ( int i = 0; i < buttons.length; i++ )
+    {
+      if ( currentButton == buttons[ i ])
+      {
+        found = true;
+        break;
+      }
+    }
+    if ( !found )
+      currentButton = null;
+
+    validate();
+  }
+
+  private void doRepaint()
+  {
+    imagePanel.repaint( 0L, 0, 0, imagePanel.getWidth(), imagePanel.getHeight());
+  }
+
+  private Button currentButton = null;
+  private JPanel imagePanel = null;
 }
