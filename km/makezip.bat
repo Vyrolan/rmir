502557c3bc8ea95507c1d729df2a4111a71fdc15
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2003-08-20 13:46:48 +0000

    Include image map files if distribution zip
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@213 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/makezip.bat
+++ km/makezip.bat
@@ -1,1 +1,1 @@
-zip RemoteMaster.%1.zip RemoteMaster.jar Setup.jar protocols.ini rmaster.bat *.txt *.ico Upgrades rdf/*.rdf images/*.jpg
+zip RemoteMaster.%1.zip RemoteMaster.jar Setup.jar protocols.ini rmaster.bat *.txt *.ico Upgrades rdf/*.rdf images/*.jpg images/*.map
