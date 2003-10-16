05e9b1afb5c10916663210ebc3502520835d96c2
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2003-10-16 17:47:41 +0000

    Remove RDF files from the distribution
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@312 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/makezip.bat
+++ km/makezip.bat
@@ -1,1 +1,1 @@
-zip RemoteMaster.%1.zip RemoteMaster.jar Setup.jar protocols.ini rmaster.bat *.txt Upgrades rdf/*.rdf images/*.jpg images/*.map images/*.ico
+zip RemoteMaster.%1.zip RemoteMaster.jar Setup.jar protocols.ini rmaster.bat *.txt Upgrades images/*.jpg images/*.map images/*.ico
