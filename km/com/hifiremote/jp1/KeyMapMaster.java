e81b053c408e90e481098a91be31c3fc68740f9b
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2004-08-10 21:30:27 +0000

    v1.03
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@467 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/com/hifiremote/jp1/KeyMapMaster.java
+++ km/com/hifiremote/jp1/KeyMapMaster.java
@@ -15,7 +15,7 @@ public class KeyMapMaster
  implements ActionListener, ChangeListener, DocumentListener
 {
   private static KeyMapMaster me = null;
-  private static final String version = "v 1.02";
+  private static final String version = "v 1.03";
   private JMenuItem newItem = null;
   private JMenuItem openItem = null;
   private JMenuItem saveItem = null;
