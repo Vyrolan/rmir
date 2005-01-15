36b77c501e7cc9b3623bdaafb6a79a8e237506b1
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2005-01-15 19:35:38 +0000

    More fixes for importing KM device upgrades that use Manual Settings.
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@523 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/com/hifiremote/jp1/Protocol.java
+++ km/com/hifiremote/jp1/Protocol.java
@@ -172,6 +172,8 @@ public class Protocol
           int pos = text.indexOf( '(' );
           int pos2 = text.indexOf( ')', pos );
           processor = text.substring( pos + 1, pos2 );
+          if ( processor.equals( "S3C8+" ))
+            processor = "S3C80";
           break;
         }
       }
