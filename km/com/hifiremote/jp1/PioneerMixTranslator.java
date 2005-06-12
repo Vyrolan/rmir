7ae6cb7185d21660e006f5c028845a5aa3ca28ce
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2005-06-12 20:32:53 +0000

    Add support for variant 3 of Pioneer MIX protocol
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@570 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/com/hifiremote/jp1/PioneerMixTranslator.java
+++ km/com/hifiremote/jp1/PioneerMixTranslator.java
@@ -11,7 +11,7 @@ public class PioneerMixTranslator
   // called to store parms into hex data
   public void in( Value[] parms, Hex hex, DeviceParameter[] devParms, int onlyIndex )
   {
-    int flag = extract( hex, 14, 2 );
+    int flag = extract( hex, 13, 3 );
     boolean doInsert = true;
     for ( int i = 0; i < parms.length; i++ )
     {
@@ -31,10 +31,8 @@ public class PioneerMixTranslator
           case 1: // Prefix cmd
             if ( val == 0 ) // none
               flag = 0;
-            else if ( val == 1 ) // cmd1
-              flag = 1;
-            else // cmd2
-              flag = 3;
+            else 
+              flag = (( val - 1 ) << 1 ) + 1;
             break;
           case 2:  // Device
             if ( val == 0 ) // dev1
@@ -49,32 +47,27 @@ public class PioneerMixTranslator
       }
     }
     if ( doInsert )
-      insert( hex, 14, 2, flag );
+      insert( hex, 13, 3, flag );
   }
 
   // called to extract parms from hex data
   public void out( Hex hex, Value[] parms, DeviceParameter[] devParms )
   {
-    int val = extract( hex, 14, 2 );
+    int val = extract( hex, 13, 3 );
     Integer zero = new Integer( 0 );
     Integer one = new Integer( 1 );
-    switch ( val )
+    if ( val == 0 ) // single cmd
     {
-      case 0:   // single
-        parms[ 0 ] = new Value( zero, null );  // PrefixDevice = none
-        parms[ 1 ] = new Value( zero, null );  // PrefixCmd = none
-        parms[ 2 ] = new Value( zero, null );  // Device = dev1
-        break;
-      case 1:  // dev1/cmd1
+      parms[ 0 ] = new Value( zero, null );  // PrefixDevice = none
+      parms[ 1 ] = new Value( zero, null );  // PrefixCmd = none
+      parms[ 2 ] = new Value( zero, null );  // Device = dev1
+    }
+    else
+    {
         parms[ 0 ] = new Value( one, null );   // PrefixDevice = dev1
-        parms[ 1 ] = new Value( one, null );   // PrefixCmd = cmd1
+        int temp = ( val >> 1 ) + 1;
+        parms[ 1 ] = new Value( new Integer( temp ), null );   // PrefixCmd = cmd1
         parms[ 2 ] = new Value( one, null );   // Device = dev2
-        break;
-      case 3: // dev1/cmd2
-        parms[ 0 ] = new Value( one, null );   // PrefixDevice = dev1
-        parms[ 1 ] = new Value( new Integer( 2 ), null );  // PrefixCmd = cmd2
-        parms[ 2 ] = new Value( one, null );   // Device = dev 2
-        break;
     }
   }
 }
