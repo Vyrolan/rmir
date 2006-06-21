e3889aa24a03948c5d281b2ba43191e260d6339d
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2006-06-21 03:25:59 +0000

    v1.59
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@618 bfc585dc-5255-483b-a5ca-a9bf5897b33d

---
+++ km/com/hifiremote/jp1/JP1Frame.java
@@ -0,0 +1,49 @@
+package com.hifiremote.jp1;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class JP1Frame extends JFrame
+{
+  public JP1Frame( String title )
+  {
+    super( title );
+    Container contentPane = super.getContentPane();
+    contentPane.add( newContentPane, BorderLayout.CENTER );
+    messageArea.setForeground( Color.red );
+    contentPane.add( messageArea, BorderLayout.SOUTH );
+  }
+  
+  public Container getContentPane()
+  {
+    return newContentPane;
+  }
+  
+  public void showMessage( String message )
+  {
+    messageArea.setText( message );
+    Toolkit.getDefaultToolkit().beep();
+  }
+  
+  public static void showMessage( String message, Component c )
+  {
+    JP1Frame frame = ( JP1Frame )SwingUtilities.getAncestorOfClass( JP1Frame.class, c );
+    if ( frame != null )
+      frame.showMessage( message );
+  }
+  
+  public void clearMessage()
+  {
+    messageArea.setText( "" );
+  }
+  
+  public static void clearMessage( Component c )
+  {
+    JP1Frame frame = ( JP1Frame )SwingUtilities.getAncestorOfClass( JP1Frame.class, c );
+    if ( frame != null )
+      frame.clearMessage();
+  }
+  
+  private JLabel messageArea = new JLabel( "" );
+  private JPanel newContentPane = new JPanel( new BorderLayout());
+}
