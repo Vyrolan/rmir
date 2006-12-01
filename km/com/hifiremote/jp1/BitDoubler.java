31d77fdd734fdc6211469dface9dd616db950374
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2006-12-01 19:26:55 +0000

    Fix extracting original value from bit-doubled value.
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@648 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/com/hifiremote/jp1/BitDoubler.java
+++ km/com/hifiremote/jp1/BitDoubler.java
@@ -1,25 +1,42 @@
 package com.hifiremote.jp1;
 
+/**
+ *  Description of the Class
+ *
+ *@author     Greg
+ *@created    December 1, 2006
+ */
 public class BitDoubler
-  extends Translate
+   extends Translate
 {
+  /**
+   *  Constructor for the BitDoubler object
+   *
+   *@param  textParms  Description of the Parameter
+   */
   public BitDoubler( String[] textParms )
   {
     super( textParms );
-    parmIndex = Integer.parseInt( textParms[ 0 ]);
-    bits = Integer.parseInt( textParms[ 1 ]);
-    offset = Integer.parseInt( textParms[ 2 ]);
+    parmIndex = Integer.parseInt( textParms[0] );
+    bits = Integer.parseInt( textParms[1] );
+    offset = Integer.parseInt( textParms[2] );
   }
-  
+
+  /**
+   *  Description of the Method
+   *
+   *@param  value  Description of the Parameter
+   *@return        Description of the Return Value
+   */
   private int doubleIt( int value )
   {
     int doubled = 0;
     int test = 1;
     int zero = 1;
     int one = 2;
-    for ( int bit = 0; bit < bits; ++ bit )
+    for ( int bit = 0; bit < bits; ++bit )
     {
-      if (( value & test ) == 0 )
+      if ( ( value & test ) == 0 )
         doubled |= zero;
       else
         doubled |= one;
@@ -27,16 +44,30 @@ public class BitDoubler
       zero <<= 2;
       one <<= 2;
     }
-    return doubled;  
+    return doubled;
   }
 
+  /**
+   *  Description of the Method
+   *
+   *@param  parms      Description of the Parameter
+   *@param  hexData    Description of the Parameter
+   *@param  devParms   Description of the Parameter
+   *@param  onlyIndex  Description of the Parameter
+   */
   public void in( Value[] parms, Hex hexData, DeviceParameter[] devParms, int onlyIndex )
   {
-    int value = (( Number )parms[ parmIndex ].getValue()).intValue();
+    int value = ( ( Number )parms[parmIndex].getValue() ).intValue();
     int doubled = doubleIt( value );
     insert( hexData, offset, bits * 2, doubled );
   }
-  
+
+  /**
+   *  Description of the Method
+   *
+   *@param  value  Description of the Parameter
+   *@return        Description of the Return Value
+   */
   private int halveIt( int value )
   {
     int halved = 0;
@@ -48,22 +79,30 @@ public class BitDoubler
       int val = value & test;
       if ( val == 2 )
         rc |= one;
-      else if (( val == 0 ) || ( val == 3 ))
+      else if ( ( val == 0 ) || ( val == 3 ) )
         System.err.println( "Error in halveIt: can't halve " + val );
-      test <<= 2;
+      value >>= 2;
       one <<= 1;
     }
     return rc;
   }
 
+  /**
+   *  Description of the Method
+   *
+   *@param  hexData   Description of the Parameter
+   *@param  parms     Description of the Parameter
+   *@param  devParms  Description of the Parameter
+   */
   public void out( Hex hexData, Value[] parms, DeviceParameter[] devParms )
   {
     int doubled = extract( hexData, offset, bits * 2 );
-    int halved = halveIt( doubled ); 
-    parms[ parmIndex ] = new Value( new Integer( halved ));
+    int halved = halveIt( doubled );
+    parms[parmIndex] = new Value( new Integer( halved ) );
   }
 
   private int parmIndex = 0;
   private int bits = 0;
   private int offset = 0;
 }
+
