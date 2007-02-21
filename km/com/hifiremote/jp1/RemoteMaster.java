dca43dcaec403397b510e3b23c39f5a0029cfc31
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2007-02-21 12:39:21 +0000

    v1.75
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@656 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/com/hifiremote/jp1/RemoteMaster.java
+++ km/com/hifiremote/jp1/RemoteMaster.java
@@ -25,7 +25,7 @@ public class RemoteMaster
   /**
    *  Description of the Field
    */
-  public final static String version = "v1.74";
+  public final static String version = "v1.75";
   private File dir = null;
   /**
    *  Description of the Field
