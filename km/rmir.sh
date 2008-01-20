783e9e89a12768104fe80970662e5113d7c137f2
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2008-01-20 18:22:40 +0000

    Linux script to launch RMIR
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@688 bfc585dc-5255-483b-a5ca-a9bf5897b33d

---
+++ km/rmir.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+len=`expr length $0`
+len=`expr $len - 8`
+if [ $len -gt 1 ]
+then
+  path=`expr substr $0 1 $len`
+else
+  path=`pwd`
+fi
+gksudo "java -Djava.library.path=$path -jar $path/RemoteMaster.jar -h $path -ir"
