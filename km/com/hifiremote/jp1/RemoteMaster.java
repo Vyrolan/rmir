a4a092499aea6a4e9cf28adc2965b8152e6325e5
gfb107 <gfb107@bfc585dc-5255-483b-a5ca-a9bf5897b33d>
2010-09-07 01:03:16 +0000

    v2.00-preview5
    git-svn-id: https://svn.code.sf.net/p/controlremote/code/trunk@921 bfc585dc-5255-483b-a5ca-a9bf5897b33d

--- km/com/hifiremote/jp1/RemoteMaster.java
+++ km/com/hifiremote/jp1/RemoteMaster.java
@@ -81,7 +81,7 @@ public class RemoteMaster extends JP1Frame implements ActionListener, PropertyCh
   private static JP1Frame frame = null;
 
   /** Description of the Field. */
-  public final static String version = "v2.00-preview4";
+  public final static String version = "v2.00-preview5";
 
   /** The dir. */
   private File dir = null;
